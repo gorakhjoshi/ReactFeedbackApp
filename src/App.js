28677c04d94963f212f6552f5ab09ec8ec3e34dd
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-03-15 21:20:09 +0545

    Header component added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import Header from './components/Header';
+
 const App = () => {
-  return <div>App</div>;
+  return <Header />;
 };
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+
+function Header({ text, bgColor }) {
+  const style = {
+    backgroundColor: bgColor,
+  };
+
+  return (
+    <div style={style}>
+      <h2>{text}</h2>
+    </div>
+  );
+}
+
+Header.defaultProps = {
+  text: 'I am a header default prop!',
+  bgColor: 'hsl(80deg 100% 50%)',
+};
+
+Header.propTypes = {
+  text: PropTypes.string,
+};
+
+export default Header;
