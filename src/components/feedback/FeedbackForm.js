ad2f5de914a9152c61b623b55b58ad53a2dc33a6
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-03-28 21:20:11 +0545

    feedback form validated

--- src/components/feedback/FeedbackForm.js
+++ src/components/feedback/FeedbackForm.js
@@ -3,11 +3,21 @@ import Card from '../common/Card';
 import Button from './../common/Button';
 
 const FeedbackForm = () => {
+  console.log('Running');
   const [text, setText] = useState('');
+  const [disabled, setDisabled] = useState(true);
+  const [message, setMessage] = useState('');
 
   function handleInputText(e) {
-    console.log(e);
     setText(e.target.value);
+
+    if (e.target.value === '' || e.target.value.trim().length <= 5) {
+      setDisabled(true);
+      setMessage('Text should be greater than 5 character');
+    } else {
+      setDisabled(false);
+      setMessage('');
+    }
   }
 
   return (
@@ -20,11 +30,11 @@ const FeedbackForm = () => {
             placeholder='Write your review'
             onChange={handleInputText}
           />
-          <Button type='submit' version='primary'>
+          <Button type='submit' version='primary' isDisabled={disabled}>
             Send
           </Button>
         </div>
-        {text}
+        {message && <p style={{ color: 'red' }}>{message}</p>}
       </form>
     </Card>
   );
