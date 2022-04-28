f4ae5f988836c40758c128ec87814a397a9411d8
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-04-28 21:41:04 +0545

    delete and put request

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -18,24 +18,44 @@ export const FeedbackProvider = ({ children }) => {
   });
 
   // Add feedback
-  const addFeedback = (newFeedback) => {
+  const addFeedback = async (newFeedback) => {
     newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
+    const response = await fetch("/feedback", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newFeedback),
+    });
+    const data = await response.json();
+    console.log(data);
+    setFeedback([data, ...feedback]);
   };
 
   const [isLoading, setIsLoading] = useState(false);
 
   // Delete feedback
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
+      await fetch(`/feedback/${id}`, { method: "DELETE" });
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
 
   // Update feedback item
-  const updateFeedback = (id, updItem) => {
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updItem),
+    });
+    const data = await response.json();
     setFeedback(
-      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+      feedback.map((item) =>
+        item.id === id ? { ...item, ...data, ...updItem } : item
+      )
     );
   };
 
@@ -50,7 +70,7 @@ export const FeedbackProvider = ({ children }) => {
   useEffect(() => {
     async function fetchFeedback() {
       setIsLoading(true);
-      const response = await fetch("http://localhost:5000/feedback");
+      const response = await fetch("/feedback");
       const data = await response.json();
       setFeedback(data);
       setIsLoading(false);
